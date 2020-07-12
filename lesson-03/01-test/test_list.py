d33f2a5556b36b1b48b867b233a394296965350d
grig22 <grig2022@ya.ru>
2020-07-12 14:01:34 +0300

    test list

---
+++ lesson-03/01-test/test_list.py
@@ -0,0 +1,33 @@
+import pytest
+
+
+def test_append():
+    ls = [1, 2, 3]
+    a = 4
+    ls.append(a)
+    assert ls == [1, 2, 3, 4]
+
+
+def test_extend():
+    ls1 = [1, 2, 3]
+    ls2 = [4, 5, 6]
+    ls1.extend(ls2)
+    assert ls1 == [1, 2, 3, 4, 5, 6]
+
+
+def test_insert():
+    ls = ['a', 'b', 'd']
+    ls.insert(2, 'c')
+    assert ls == ['a', 'b', 'c', 'd']
+
+
+def test_remove():
+    ls = [111, 222, 333, 444, 555]
+    ls.remove(444)
+    assert ls == [111, 222, 333, 555]
+
+@pytest.mark.parametrize("pos, exp", [(1, 911), (2, 922), (3, 933)])
+def test_pop(pos, exp):
+    ls = [900, 911, 922, 933]
+    val = ls.pop(pos)
+    assert val == exp
