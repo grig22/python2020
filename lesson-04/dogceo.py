72dd4070b9ad12dbe23653df71d217e3cacb0107
grig22 <grig2022@ya.ru>
2020-07-26 14:32:01 +0300

    не работает format для URL

--- lesson-04/dogceo.py
+++ lesson-04/dogceo.py
@@ -4,17 +4,42 @@ import requests
 
 def test_random_image():
     res = requests.get("https://dog.ceo/api/breeds/image/random")
-    assert res.json()["status"] == "success"
+    res_json = res.json()
+    assert res_json["status"] == "success"
 
 
 def breeds():
     res = requests.get("https://dog.ceo/api/breeds/list/all")
     res_json = res.json()
     assert res_json["status"] == "success"
-    return res_json["message"].keys()
+    msg = res_json["message"]
+    assert type(msg) is dict
+    assert msg
+    return msg
+
+
+def all_breeds():
+    return breeds().keys()
+
+
+def breeds_with_subs():
+    res = []
+    for key, val in breeds():
+        if val:
+            res.append(key)
+    assert res
+    return res
 
 
 @pytest.mark.parametrize("breed", breeds())
 def test_by_breed(breed):
-    print(breed)
-
+    t = "111{}222".format("finnish")
+    assert t == "111finnish222"
+    addr = "https://dog.ceo/api/{}/hound/images".format("finnish")
+    # res = requests.get("https://dog.ceo/api/{}/hound/images".format('finnish'))
+    res = requests.get(addr)
+    res_json = res.json()
+    # assert res_json["status"] == "success"
+    # msg = res_json["message"]
+    # assert type(msg) is list
+    # assert msg
