ebeef4fdc68de8e702324b481ed992f045fb4156
grig22 <grig2022@ya.ru>
2020-08-04 16:23:20 +0300

    заготовка add_square

--- lesson-06/shape.py
+++ lesson-06/shape.py
@@ -4,8 +4,33 @@ class Shape:
     def name(self):
         return self.__class__.__name__
 
+    @property
+    def area(self):
+        return 33
+
+    @property
+    def angles(self):
+        return 44
+
+    @property
+    def perimeter(self):
+        return 55
+
+    def add_square(self, other):
+        if not isinstance(other, Shape):
+            raise TypeError("Надо 'Shape', а это '{}'".format(type(other).__name__))
+        return self.area + other.area
+
 
 class Triangle(Shape):
+    pass
+
+
+class Rectangle(Shape):
+    pass
+
+
+class Circle(Shape):
     pass
 
 
@@ -13,4 +38,10 @@ s = Shape()
 t = Triangle()
 
 print(s.name)
+print(s.area)
+
 print(t.name)
+print(t.area)
+
+print(s.add_square(t))
+print(t.add_square([55,66,77,88]))
