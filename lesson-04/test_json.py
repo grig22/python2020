4cd2ac3af7630c541b984c38591d9dc81ebb66c1
grig22 <grig2022@ya.ru>
2020-08-07 14:47:23 +0300

    test patch

--- lesson-04/test_json.py
+++ lesson-04/test_json.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 from jsonschema import validate
-# import json
+from copy import deepcopy
 
 
 class SCHEMA:
@@ -16,7 +16,7 @@ class SCHEMA:
         "required": ["userId", "id", "title", "body"]
     }
 
-    USERID = {
+    USER_ID = {
             "type": "object",
             "properties": {
                 "id": {"type": "number"},
@@ -26,10 +26,11 @@ class SCHEMA:
 
 
 def test_schema_posts():
+    posts_in_feed = 100
     schema = {
         "type": "array",
-        "minItems": 100,
-        "maxItems": 100,
+        "minItems": posts_in_feed,
+        "maxItems": posts_in_feed,
         "items": SCHEMA.POST
     }
     res = requests.get("https://jsonplaceholder.typicode.com/posts")
@@ -37,32 +38,68 @@ def test_schema_posts():
     validate(instance=res_json, schema=schema)
 
 
-def userids():
+def fetch_user_ids():
+    users_in_feed = 10
     schema = {
         "type": "array",
-        "minItems": 10,
-        "maxItems": 10,
-        "items": SCHEMA.USERID
+        "minItems": users_in_feed,
+        "maxItems": users_in_feed,
+        "items": SCHEMA.USER_ID
     }
     res = requests.get("https://jsonplaceholder.typicode.com/users")
     res_json = res.json()
     validate(instance=res_json, schema=schema)
-    ids = set()
+    ids = list()
     for user in res_json:
-        ids.add(user["id"])
+        ids.append(user["id"])
     assert ids
     return ids
 
 
-@pytest.mark.parametrize("userid", userids())
-def test_create(userid):
+@pytest.mark.parametrize("user_id", fetch_user_ids())
+def test_create(user_id):
     body = {
         "title": "Тестирование REST сервиса 3",
         "body": "Тесты должны успешно проходить.",
-        "userId": userid
+        "userId": user_id
     }
     res = requests.post("https://jsonplaceholder.typicode.com/posts", json=body)
     res_json = res.json()
     assert body.items() <= res_json.items()
-    assert res_json["id"] == 101
+    new_post_id = 101
+    assert res_json["id"] == new_post_id
 
+
+def fetch_post_ids(user_id):
+    res = requests.get("https://jsonplaceholder.typicode.com/posts", params={"userId": user_id})
+    ids = list()
+    res_json = res.json()
+    assert res_json
+    for post in res_json:
+        validate(instance=post, schema=SCHEMA.POST)
+        ids.append(post["id"])
+    assert ids
+    return ids
+
+
+some_bodies = ["Тесты должны успешно проходить.",
+               "",
+               """The C++ Programming Language, Fourth Edition, delivers meticulous,
+richly explained, and integrated coverage of the entire language — its facilities,
+abstraction mechanisms, standard libraries, and key design techniques.""",
+               "HUGE TEXT " * 1024]
+
+
+@pytest.mark.parametrize("body", some_bodies)
+@pytest.mark.parametrize("post_id", fetch_post_ids(fetch_user_ids()[0])[0:2])
+def test_patch(post_id, body):
+    url = "https://jsonplaceholder.typicode.com/posts/{}".format(post_id)
+    old = requests.get(url)
+    old_json = old.json()
+    validate(instance=old_json, schema=SCHEMA.POST)
+    new_json = deepcopy(old_json)
+    new_json["body"] = body
+    res = requests.patch(url, json=new_json)
+    res_json = res.json()
+    validate(instance=res_json, schema=SCHEMA.POST)
+    assert res_json == new_json
