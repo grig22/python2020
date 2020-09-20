ab8e9de24525812754f2cb4aa0906bcf537714dd
grig22 <grig2022@ya.ru>
2020-09-20 19:04:43 +0300

    test_product_list

--- lesson-08/test_wait.py
+++ lesson-08/test_wait.py
@@ -2,11 +2,12 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+# https://www.selenium.dev/documentation/en/webdriver/waits/
 
 
-# 3.1
+# 3.1 Добавить проверку логина и разлогина раздела администратора.
 def test_login_admin(browser):
-    # https://www.selenium.dev/documentation/en/webdriver/waits/
     browser.get(browser.url + "/admin/")
     assert browser.title == "Administration"
     username = browser.find_element_by_id("input-username")
@@ -25,3 +26,12 @@ def test_login_admin(browser):
     # разлогинимся
     browser.find_element_by_link_text("Logout").click()
     wait.until(EC.title_is("Administration"))
+
+
+# 3.2 Добавить проверку перехода к разделу с товарами, что появляется таблица с товарами.
+def test_product_list(browser):
+    browser.get(browser.url + "/index.php?route=product/category&path=18")
+    wait = WebDriverWait(browser, timeout=3)
+    wait.until(EC.title_is("Laptops & Notebooks"))
+    wait.until(EC.visibility_of_element_located((By.ID, "content")))
+    wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "MacBook Pro")))
