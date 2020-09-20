f31b472ced33b12b0cd5540cb56563358e5c655a
grig22 <grig2022@ya.ru>
2020-09-20 18:50:04 +0300

    test_login_admin

--- lesson-08/test_wait.py
+++ lesson-08/test_wait.py
@@ -1,6 +1,6 @@
 import time
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -8,13 +8,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 def test_login_admin(browser):
     # https://www.selenium.dev/documentation/en/webdriver/waits/
     browser.get(browser.url + "/admin/")
+    assert browser.title == "Administration"
     username = browser.find_element_by_id("input-username")
     password = browser.find_element_by_id("input-password")
+    # логинимся
     username.send_keys("admin")
     password.send_keys("1" + Keys.ENTER)
-    assert browser.title == "Dashboard"
-    WebDriverWait(browser, timeout=3).until(lambda bro: bro.find_element_by_link_text("Logout"))
-
-
-
-
+    # загрузилось
+    wait = WebDriverWait(browser, timeout=3)
+    wait.until(EC.title_is("Dashboard"))
+    wait.until(lambda bro: bro.find_element_by_link_text("Logout"))
+    # проверяем что это я
+    profile = browser.find_element_by_id("user-profile")
+    assert profile.get_attribute("alt") == "Benedict Cumberbatch"
+    assert profile.get_property("alt") == "Benedict Cumberbatch"
+    # разлогинимся
+    browser.find_element_by_link_text("Logout").click()
+    wait.until(EC.title_is("Administration"))
