d8accc6f203af99954acdbdf464c4e23ba1ba6b8
grig22 <grig2022@ya.ru>
2020-09-26 14:47:00 +0300

    test_login_admin

--- lesson-12/basepage.py
+++ lesson-12/basepage.py
@@ -1,16 +1,36 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 class BasePage:
 
     def __init__(self, driver, url=""):
         self.driver = driver
+        self.webdr_wait = WebDriverWait(self.driver, timeout=3)
         self.driver.get(self.driver.url + "/" + url)
 
-    def find_elem(self, value, method = "id"):
+    def find_elem(self, value, method="id"):
         if method == "id":
             return self.driver.find_element_by_id(value)
         elif method == "link":
             return self.driver.find_element_by_link_text(value)
         else:
-            return None
+            raise ValueError(f'unsupported method "{method}"')
 
     def title(self):
         return self.driver.title
+
+    def wait_title(self, title):
+        self.webdr_wait.until(EC.title_is(title))
+
+    def wait_elem(self, value, method="id"):
+        """If the condition fails, e.g. a truthful return value from the condition is never reached,
+         the wait will throw/raise an error/exception called a timeout error."""
+        if method == "id":
+            locator = By.ID
+        elif method == "link":
+            locator = By.LINK_TEXT
+        else:
+            raise ValueError(f'unsupported method "{method}"')
+        self.webdr_wait.until(EC.visibility_of_element_located((locator, value)))
--- lesson-12/test_wait.py
+++ lesson-12/test_wait.py
@@ -1,30 +1,31 @@
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-# https://www.selenium.dev/documentation/en/webdriver/waits/
+from basepage import BasePage
 
 
-# 3.1 Добавить проверку логина и разлогина раздела администратора.
+# Добавить проверку логина и разлогина раздела администратора.
 def test_login_admin(browser):
-    browser.get(browser.url + "/admin/")
-    assert browser.title == "Administration"
-    username = browser.find_element_by_id("input-username")
-    password = browser.find_element_by_id("input-password")
+    admin_page = BasePage(browser, "/admin/")
+    assert admin_page.title() == "Administration"
+    username = admin_page.find_elem("input-username")
+    password = admin_page.find_elem("input-password")
     # логинимся
     username.send_keys("admin")
     password.send_keys("1" + Keys.ENTER)
     # загрузилось
-    wait = WebDriverWait(browser, timeout=3)
-    wait.until(EC.title_is("Dashboard"))
-    wait.until(lambda bro: bro.find_element_by_link_text("Logout"))
+    # wait = WebDriverWait(browser, timeout=3)
+    admin_page.wait_title("Dashboard")
+    # wait.until(EC.title_is("Dashboard"))
+    admin_page.wait_elem(method="link", value="Logout")
+    # wait.until(lambda bro: bro.find_element_by_link_text("Logout"))
     # проверяем что это я
-    profile = browser.find_element_by_id("user-profile")
+    profile = admin_page.find_elem("user-profile")
+    # profile = browser.find_element_by_id("user-profile")
     assert profile.get_attribute("alt") == "Benedict Cumberbatch"
-    assert profile.get_property("alt") == "Benedict Cumberbatch"
     # разлогинимся
-    browser.find_element_by_link_text("Logout").click()
-    wait.until(EC.title_is("Administration"))
+    admin_page.find_elem(method="link", value="Logout").click()
+    # browser.find_element_by_link_text("Logout").click()
+    admin_page.wait_title("Administration")
+    # wait.until(EC.title_is("Administration"))
 
 
 # 3.2 Добавить проверку перехода к разделу с товарами, что появляется таблица с товарами.
