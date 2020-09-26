6fa1fa717e243a3d55ee8c721fb7cdc4ff723c65
grig22 <grig2022@ya.ru>
2020-09-26 14:56:24 +0300

    some cleanup

--- lesson-12/test_wait.py
+++ lesson-12/test_wait.py
@@ -12,30 +12,19 @@ def test_po_login_admin(browser):
     username.send_keys("admin")
     password.send_keys("1" + Keys.ENTER)
     # загрузилось
-    # wait = WebDriverWait(browser, timeout=3)
     admin_page.wait_title("Dashboard")
-    # wait.until(EC.title_is("Dashboard"))
     admin_page.wait_elem(method="link", value="Logout")
-    # wait.until(lambda bro: bro.find_element_by_link_text("Logout"))
     # проверяем что это я
     profile = admin_page.find_elem("user-profile")
-    # profile = browser.find_element_by_id("user-profile")
     assert profile.get_attribute("alt") == "Benedict Cumberbatch"
     # разлогинимся
     admin_page.find_elem(method="link", value="Logout").click()
-    # browser.find_element_by_link_text("Logout").click()
     admin_page.wait_title("Administration")
-    # wait.until(EC.title_is("Administration"))
 
 
-# 3.2 Добавить проверку перехода к разделу с товарами, что появляется таблица с товарами.
+# Добавить проверку перехода к разделу с товарами, что появляется таблица с товарами.
 def test_po_product_list(browser):
-    # browser.get(browser.url + "/index.php?route=product/category&path=18")
     product_page = BasePage(browser, "/index.php?route=product/category&path=18")
-    # wait = WebDriverWait(browser, timeout=3)
-    # wait.until(EC.title_is("Laptops & Notebooks"))
     product_page.wait_title("Laptops & Notebooks")
-    # wait.until(EC.visibility_of_element_located((By.ID, "content")))
     product_page.wait_elem("content")
-    # wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "MacBook Pro")))
     product_page.wait_elem(method="link", value="MacBook Pro")
