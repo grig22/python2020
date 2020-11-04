cefeb226f69803163fb0d24faab8a0aa0cde085c
grig22 <grig2022@ya.ru>
2020-11-04 16:32:18 +0300

    некоторые усовершенствования

--- lesson-19/parser.py
+++ lesson-19/parser.py
@@ -12,25 +12,43 @@
 # %O	Bytes sent, including headers.
 # %{VARNAME}i	The contents of VARNAME: header line(s) in the request sent to the server.
 
+# а этого параметра нет в моих логах
+# топ 10 самых долгих запросов, должно быть видно метод, url, ip, время запроса
+# %T	The time taken to serve the request, in seconds.
+
 import re
 from collections import defaultdict
 
 filename = "/var/log/apache2/access.log"
-sample = '::1 - - [04/Nov/2020:13:00:49 +0300] "GET // HTTP/1.1" 200 4482 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"'
 regex = '(.*?) (.*?) (.*?) \[(.*?)\] "(.*?)" (\d*) (\d*) "(.*?)" "(.*?)"'
 
-pos_request = 5
+pos_ip = 0
+pos_request = 4
+pos_status = 5
+client_error = '4..'
+server_error = '5..'
 
-def request_type(parsed):
-    request = parsed.group(pos_request)
-    found = re.match('\A\D+? ', request).group()
-    if not found:
-        print('BAD REQUEST', request)
-        assert 0
-    return found
+request_overall = 0  # общее количество выполненных запросов
+ip_addrs = defaultdict(int)  # топ 10 IP адресов, с которых были сделаны запросы
+request_by_type = defaultdict(int)  # количество запросов по типу: GET - 20, POST - 10 и т.п.
+request_client_err = list()
+request_server_err = list()
 
-request_overall = 0
-request_by_type = defaultdict(int)
+
+def split_request(request_line):
+    gr = re.match('(\A[A-Z]+?) (.+?) ', request_line).groups()
+    return {'met': gr[0], 'url': gr[1]}
+
+
+# должно быть видно метод, url, статус код, ip адрес
+def extract_data(parsed_mobj):
+    pg = parsed_mobj.groups()
+    split = split_request(pg[pos_request])
+    return (split['met'],
+            split['url'],
+            pg[pos_status],
+            pg[pos_ip])
+
 
 lines = open(filename, 'r').readlines()
 for line in lines:
@@ -38,7 +56,24 @@ for line in lines:
     if not parsed:
         print('BAD LINE', line)
         assert 0
-    rt = request_type(parsed)
+    groups = parsed.groups()
+    # общее количество выполненных запросов
     request_overall += 1
-    request_by_type[rt] += 1
+    # топ 10 IP адресов, с которых были сделаны запросы
+    ip_addrs[groups[pos_ip]] += 1
+    # количество запросов по типу: GET - 20, POST - 10 и т.п.
+    request_type = split_request(groups[pos_request])['met']
+    request_by_type[request_type] += 1
+    # топ 10 не получится, потому что не задан критерий сортировки, значит будут просто первые 10
+    # топ 10 запросов, которые завершились клиентской ошибкой, должно быть видно метод, url, статус код, ip адрес
+    if re.match(client_error, groups[pos_status]):
+        request_client_err.append(extract_data(parsed))
+    # топ 10 запросов, которые завершились ошибкой со стороны сервера, должно быть видно метод, url, статус код, ip адрес
+
+ip_addrs_sorted = {k: v for k, v in sorted(ip_addrs.items(), key=lambda item: item[1])}
+ip_items = ip_addrs_sorted.items()
+ip_first_ten = list(ip_items)[:10]
+print(request_overall)
 print(request_by_type)
+print(ip_first_ten)
+print(request_client_err)
